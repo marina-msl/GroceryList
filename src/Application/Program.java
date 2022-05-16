8a7594544298a93da0e5eb6c2598ebb49afa68fe
Marina Leide <marina.msleide@gmail.com>
2022-05-16 00:20:31 -0300

    Implementing recovery data from products table

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -5,16 +5,9 @@
 
 package Application;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 import java.util.Scanner;
 
-import GUI.ProductsView;
-import db.DB;
-import db.DbException;
 import model.dao.DaoFactory;
 import model.dao.ProductDao;
 import model.entities.Product;
@@ -50,30 +43,12 @@ public class Program {
 		
 		
 		// --------------> Recovering data
+		 System.out.println("Product List");
+		List<Product> list = productDao.findAll();
+		for (Product plist : list) {
+			System.out.println(plist.getName());
+		}
 		
-		Connection conn = null;
-		
-		Statement st = null;
-		ResultSet rs = null;
-
-			try{
-				conn = DB.getConnection();
-				st = conn.createStatement();
-				rs = st.executeQuery("select * from product");
-		
-				while (rs.next()) {
-					System.out.println("Name product: " + rs.getString("NAME") + "\nAmount product: "
-							+ rs.getInt("AMOUNT") + "\nPrice product: " + rs.getDouble("PRICE"));
-				}
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-			}
-			finally	{
-				DB.closeResultSet(rs);
-				DB.closeStatement(st);
-				DB.closeConnection();
-			}
 			
 			
 			// ----------------> Requesting which product should be delete in database
--- src/model/dao/impl/ProductDaoJDBC.java
+++ src/model/dao/impl/ProductDaoJDBC.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
-
-import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
@@ -75,8 +75,34 @@ public class ProductDaoJDBC implements ProductDao{
 
 	@Override
 	public List<Product> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try{
+		
+			st = conn.prepareStatement("select * from product");
+			rs = st.executeQuery();
+			
+			List<Product> list = new ArrayList<>();
+		
+			while (rs.next()) {
+				Product obj = new Product(null, 0, 0);
+				obj.setId(rs.getInt("id"));
+				obj.setName(rs.getString("name"));
+				obj.setPrice(rs.getDouble("price"));
+				obj.setAmount(rs.getInt("amount"));
+				list.add(obj);
+			}
+			return list;
+		}
+		catch(SQLException e){
+			throw new DbException(e.getMessage());
+		}
+		finally	{
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 	
 	
